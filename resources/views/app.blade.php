41360abd3802b2f00646da10f455032d9ab170d2
itschool <itschoolgithub@mail.ru>
2026-05-30 11:55:08 +0500

    build

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="">
+  <head>
+    <meta charset="UTF-8">
+    <link rel="icon" href="/favicon.ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vite App</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+    <script type="module" crossorigin src="/assets/index-Bysay_ab.js"></script>
+    <link rel="stylesheet" crossorigin href="/assets/index-DG1KKhCg.css">
+  </head>
+  <body>
+    <div id="app"></div>
+
+  </body>
+</html>
